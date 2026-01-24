3a5786c064d2d5c48b4f7308a45e3069989ada5e
Davide Ferrari <vide@fastmail.com>
2026-01-24 21:38:27 +0100

    chore: release v0.12.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.matedroid"
         minSdk = 28
         targetSdk = 35
-        versionCode = 24
-        versionName = "0.12.0"
+        versionCode = 25
+        versionName = "0.12.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
